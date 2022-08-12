205bfcab072af8a7539d15263b1bb6083d6fa9c2
IlliterateElf <illiterate.elf@outlook.com>
2022-08-11 20:38:24 -0700

    js: playing with objects

--- javascript/code.js
+++ javascript/code.js
@@ -1,1 +1,18 @@
-alert('Hello, world!');
+"use strict";
+
+let user = {};
+
+let result = '';
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+for (let key in user) {
+    result = result + key + ' is ' + user[key] + ' ';
+}
+alert(result);
+result = '';
+delete user.name;
+for (let key in user) {
+    result = result + key + ' is ' + user[key] + ' ';
+}
+alert(result);
