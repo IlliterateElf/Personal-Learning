b2e575b2e8edcdce2d75ab5a32c827115fdf29b8
IlliterateElf <illiterate.elf@outlook.com>
2022-08-13 15:20:04 -0700

    react: login.js configured with issues
    Getting errors about function components cannot be given refs, seems to be something with the new version

--- javascript/e-commerce/src/App.js
+++ javascript/e-commerce/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
 import AddProduct from './components/AddProduct';
 import Cart from './components/Cart';
 import Login from './components/Login';
 import ProductList from './components/ProductList';
 
 import Context from "./Context";
+
 
 export default class App extends Component {
   constructor(props) {
@@ -18,6 +22,41 @@ export default class App extends Component {
     };
     this.routerRef = React.createRef();
   }
+
+  componentDidMount() {
+    let user = localStorage.getItem("user");
+    user = user ? JSON.parse(user) : null;
+    this.setState({ user });
+  }
+
+  login = async (email, password) => {
+    const res = await axios.post(
+      { email, password },
+    ).catch((res) => {
+      return { status: 401, message: 'Unauthorized' }
+    })
+  
+    if(res.status === 200) {
+      const { email } = jwt_decode(res.data.accessToken)
+      const user = {
+        email,
+        token: res.data.accessToken,
+        accessLevel: email === 'admin@example.com' ? 0 : 1
+      }
+  
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    } else {
+      return false
+    }
+  }
+
+  logout = e => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
 
   render() {
     return (
