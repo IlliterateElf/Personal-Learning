0bc6486acc9dcb6565e27859cf93a39a11e63ca4
IlliterateElf <illiterate.elf@outlook.com>
2022-08-13 14:13:40 -0700

    react: App.js config with issues
    AddProduct is having some issues once loaded, says undefined

--- javascript/e-commerce/src/App.js
+++ javascript/e-commerce/src/App.js
@@ -1,25 +1,99 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from "react";
+import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
-export default App;
+import AddProduct from './components/AddProduct';
+import Cart from './components/Cart';
+import Login from './components/Login';
+import ProductList from './components/ProductList';
+
+import Context from "./Context";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: []
+    };
+    this.routerRef = React.createRef();
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart.AddProduct,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className='App'>
+            <nav
+            className='navbar container'
+            role='navigation'
+            aria-label='main navigation'
+            >
+              <div className='navbar-brand'>
+                <b className='navbar-item is-size-4 '>ecommerce</b>
+                <label
+                  role='button'
+                  class='navbar-burger burger'
+                  aria-label='menu'
+                  aria-expanded='false'
+                  data-target='navbarBasicExample'
+                  onClick={e => {
+                    e.preventDefault();
+                    this.setState({ showMenu: !this.state.showMenu });
+                  }}
+                >
+                  <span aria-hidden="true"></span>
+                  <span aria-hidden='true'></span>
+                  <span aria-hidden='true'></span>
+                  <span aria-hidden='true'></span>
+                </label>
+              </div>
+              <div className={`navbar-menu ${
+                this.state.showMenu ? 'is-active': ""
+              }`}>
+                <Link to='/cart' className='navbar-item'>
+                  Cart
+                  <span
+                    className='tag is-primary'
+                    style={{ marginLeft: '5px' }}
+                  >
+                    { Object.keys(this.state.cart).length }
+                  </span>
+                </Link>
+                {!this.state.user ? (
+                  <Link to='/login' className='navbar-item'>
+                    Login
+                  </Link>
+                ) : (
+                  <Link to='/' onClick={this.logout} className='navbar-item'>
+                    Logout
+                  </Link>
+                )}
+              </div>
+            </nav>
+            <Routes>
+              <Route exact path='/' component={ProductList} />
+              <Route exact path='/login' component={Login} />
+              <Route exact path='/cart' component={Cart} />
+              <Route exact path='/add-product' component={AddProduct} />
+              <Route exact path='/products' component={ProductList} />
+            </Routes>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
+}
