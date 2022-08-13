9976707ea3d1ec64fdf476fd7c16a2457528f204
IlliterateElf <illiterate.elf@outlook.com>
2022-08-13 14:18:06 -0700

    react: App.js fixed
    Found issue. It was that <Link to="/add-product" hadn't been done due to missing <Link> portions.

--- javascript/e-commerce/src/App.js
+++ javascript/e-commerce/src/App.js
@@ -1,8 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 import React, { Component } from "react";
-import { Routes, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
 
 import AddProduct from './components/AddProduct';
 import Cart from './components/Cart';
@@ -28,7 +25,7 @@ export default class App extends Component {
         value={{
           ...this.state,
           removeFromCart: this.removeFromCart,
-          addToCart: this.addToCart.AddProduct,
+          addToCart: this.addToCart,
           login: this.login,
           addProduct: this.addProduct,
           clearCart: this.clearCart,
@@ -36,61 +33,68 @@ export default class App extends Component {
         }}
       >
         <Router ref={this.routerRef}>
-          <div className='App'>
-            <nav
-            className='navbar container'
-            role='navigation'
-            aria-label='main navigation'
-            >
-              <div className='navbar-brand'>
-                <b className='navbar-item is-size-4 '>ecommerce</b>
-                <label
-                  role='button'
-                  class='navbar-burger burger'
-                  aria-label='menu'
-                  aria-expanded='false'
-                  data-target='navbarBasicExample'
-                  onClick={e => {
-                    e.preventDefault();
-                    this.setState({ showMenu: !this.state.showMenu });
-                  }}
-                >
-                  <span aria-hidden="true"></span>
-                  <span aria-hidden='true'></span>
-                  <span aria-hidden='true'></span>
-                  <span aria-hidden='true'></span>
-                </label>
-              </div>
+        <div className="App">
+          <nav
+            className="navbar container"
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbar-brand">
+              <b className="navbar-item is-size-4 ">ecommerce</b>
+              <label
+                role="button"
+                class="navbar-burger burger"
+                aria-label="menu"
+                aria-expanded="false"
+                data-target="navbarBasicExample"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ showMenu: !this.state.showMenu });
+                }}
+              >
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+              </label>
+            </div>
               <div className={`navbar-menu ${
-                this.state.showMenu ? 'is-active': ""
-              }`}>
-                <Link to='/cart' className='navbar-item'>
+                  this.state.showMenu ? "is-active" : ""
+                }`}>
+                <Link to="/products" className="navbar-item">
+                  Products
+                </Link>
+                {this.state.user && this.state.user.accessLevel < 1 && (
+                  <Link to="/add-product" className="navbar-item">
+                    Add Product
+                  </Link>
+                )}
+                <Link to="/cart" className="navbar-item">
                   Cart
                   <span
-                    className='tag is-primary'
-                    style={{ marginLeft: '5px' }}
+                    className="tag is-primary"
+                    style={{ marginLeft: "5px" }}
                   >
                     { Object.keys(this.state.cart).length }
                   </span>
                 </Link>
                 {!this.state.user ? (
-                  <Link to='/login' className='navbar-item'>
+                  <Link to="/login" className="navbar-item">
                     Login
                   </Link>
                 ) : (
-                  <Link to='/' onClick={this.logout} className='navbar-item'>
+                  <Link to="/" onClick={this.logout} className="navbar-item">
                     Logout
                   </Link>
                 )}
               </div>
             </nav>
-            <Routes>
-              <Route exact path='/' component={ProductList} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/cart' component={Cart} />
-              <Route exact path='/add-product' component={AddProduct} />
-              <Route exact path='/products' component={ProductList} />
-            </Routes>
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
           </div>
         </Router>
       </Context.Provider>
