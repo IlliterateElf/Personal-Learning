0bda847b8bdb8e362bcfaf097571530e99359a26
IlliterateElf <illiterate.elf@outlook.com>
2022-08-13 20:35:33 -0700

    react: ShoppingCart init

--- javascript/my-first-app/src/App.jsx
+++ javascript/my-first-app/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import CustomersList from "./CustomersList";
 import NavBar from "./NavBar";
+import ShoppingCart from "./ShoppingCart";
 
 export default class App extends Component {
     render() {
         return (
             <React.Fragment>
                 <NavBar />
-                <CustomersList />
+                <ShoppingCart />
             </React.Fragment>
         );
     }
--- javascript/my-first-app/src/NavBar.jsx
+++ javascript/my-first-app/src/NavBar.jsx
@@ -6,7 +6,7 @@ export class NavBar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-style">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/#">My App</a>
+                        <a className="navbar-brand" href="/#">eCommerce</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
